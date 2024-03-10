bd116023bed9f7d32e7db2362a4631386797a04f
sumitdhyani <sumitdhyani1@gmail.com>
2024-03-10 21:02:35 +0530

    1st Working version of Express.ts

---
+++ infra/io/Express.ts
@@ -0,0 +1,30 @@
+// Import required modules
+import { ErrCallback, Logger, ReqRespIOUtilsCallback } from '../PublicInterfaces';
+import express, { Request, Response } from 'express';
+export { ErrCallback, ReqRespIOUtilsCallback } from '../PublicInterfaces'
+
+export type ReqHandler = (queryParams: Object, body: Object, headers: Object, params: Object, resultCallback: (result: Object)=>void) => void;
+
+export function initExpress (port: number, logger: Logger, reqRespIOUtilsCallback: ReqRespIOUtilsCallback, errCallback: ErrCallback) : void {
+  const app = express();
+  app.listen(port, () => {
+    logger.info(`Server is listening on port ${port}`);
+  });
+
+  let existingRoutes: Set<string> = new Set<string>();
+  function setRoute(route: string, reqHandler: ReqHandler, errCallback: ErrCallback) : void {
+    // Check if route is already set, if so, throw an error.
+    if( existingRoutes.has(route) ) {
+      errCallback(new Error("This route has already been set"))
+    }
+    existingRoutes.add(route);
+    // Define new route handler using reqHandler function.
+    app.get(route, (req: Request, res: Response) => {
+     // Call reqHandler with request's query, body, headers, and params.
+     // Use resultHandler to send result back to client.
+     reqHandler(req.query, req.body, req.headers, req.params, (result: Object) => { res.send(result) })
+    })
+  }
+
+  reqRespIOUtilsCallback({setRoute : setRoute})
+}
